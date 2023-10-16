f865b6a4c0a60d89e508c5873c08d46503f546cd
Dhairyaa2004 <100464678+Dhairyaa2004@users.noreply.github.com>
2023-10-16 22:41:36 +0530

    odd hello world dhairya

--- c/oddEven.c
+++ c/oddEven.c
@@ -1,21 +1,17 @@
 /*calculating weather number is odd or even without using modulas (%) operator*/
 
-#include<stdio.h>
 
-int oddEven(int number) {
-    int temp = number;
+#include <stdio.h> 
 
-    temp = temp /2 ;
-    
-    (temp * 2 == number) ? printf("%d is Even", number) : printf("%d is Odd", number);
 
-}
-
-int main () {
-
-    int number,temp;
-    printf("Enter the number: ");
-    scanf("%d", &number);
-    oddEven(number);
+int main()
+{
+    int n;
+    printf("enter a number");
+    scanf("%d",&n);
+    if(n%2==0)
+    printf("Number is even");
+    else
+    printf("Number is odd");
     return 0;
 }
